2ef462136911174d496d759737d87c9d22025fe6
Diaa Mo <diaam917@gmail.com>
2023-02-11 20:31:59 +0200

    c

---
+++ database/migrations/2023_02_11_181659_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->email('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('password');
+            $table->int('age');
+            $table->char('gender');
+            $table->boolean('graduation_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
