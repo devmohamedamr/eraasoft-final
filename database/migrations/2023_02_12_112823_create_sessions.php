2a3d781e40b1d9f4897a5adfe4d996e3cfc6c3e5
khaledfathi <khaledfathi@protonmail.com>
2023-02-12 14:00:03 +0200

    khaledfathi_migrations

---
+++ database/migrations/2023_02_12_112823_create_sessions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->nullable(false)->references('id')->on('groups');
+            $table->date('date')->nullable(false);
+            $table->time('time')->nullable(false);
+            $table->integer('duration')->nullable(false);
+            $table->integer('room_number')->nullable(false);
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
